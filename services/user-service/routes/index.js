f59d6175391a445e81ba06084c1904bc85caf6e0
Gabriel Goh <77230723+gycgabriel@users.noreply.github.com>
2023-09-11 15:47:59 +0800

    Test db

--- services/user-service/routes/index.js
+++ services/user-service/routes/index.js
@@ -4,7 +4,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', async(req, res, next) => {
-  const result = await db.query('SELECT NOW()')
+  const result = await db.query('SELECT * FROM users.users;')
   res.json(result);
 });
 
