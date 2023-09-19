192ce0bcbc3f657f61233bb8fd27775f575eb166
Charisma Kausar <charisma.kausar@gmail.com>
2023-09-20 04:58:54 +0800

    style: Add fonts for new routes

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,11 +1,26 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/layout'
+import { Noto_Sans } from 'next/font/google'
+
+const notoSans = Noto_Sans({
+  weight: ['400', '500', '600', '700', '800', '900'],
+  preload: false
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <style jsx global>{`
+        :root {
+          --font_noto_sans: ${notoSans.style.fontFamily};
+        }
+      `}</style>
+      <main>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </main>
+    </>
   )
 }
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,33 +1,23 @@
 import { Button } from '@/components/ui/button'
 import Spotlight, { SpotlightCard } from '@/components/ui/spotlight'
 import { TypographyH1, TypographyH3 } from '@/components/ui/typography'
-import { Noto_Sans } from 'next/font/google'
 import Image from 'next/image'
 import { AiFillGithub } from 'react-icons/ai'
 
-const notoSans = Noto_Sans({
-  weight: ['400', '500', '600', '700', '800', '900'],
-  preload: false
-})
-
 export default function Home() {
   return (
-    <main
-      className={`${notoSans.className}`}
-    >
-      <Spotlight className="group">
-        <SpotlightCard>
-          <div className="p-24 mt-12 flex flex-col items-center justify-center text-center">
-            <TypographyH1 className="text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary mb-4">Elevate your technical interview prep.</TypographyH1>
-            <TypographyH3 className="text-foreground mb-8">Crush technical interviews by polishing your skills with friends.</TypographyH3>
-            <Button className="mb-16">
-              <AiFillGithub className="mr-2 w-6 h-6" />
-              Sign up with GitHub
-            </Button>
-            <Image src={'/hero-image.jpeg'} width={800} height={600} alt="Screenshot of the collaboration feature" className="shadow-2xl shadow-secondary/20 shadow-ring-10"/>
-          </div>
-        </SpotlightCard>
-      </Spotlight>
-    </main>
+    <Spotlight className="group">
+      <SpotlightCard>
+        <div className="p-24 mt-12 flex flex-col items-center justify-center text-center">
+          <TypographyH1 className="text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary mb-4">Elevate your technical interview prep.</TypographyH1>
+          <TypographyH3 className="text-foreground mb-8">Crush technical interviews by polishing your skills with friends.</TypographyH3>
+          <Button className="mb-16">
+            <AiFillGithub className="mr-2 w-6 h-6" />
+            Sign up with GitHub
+          </Button>
+          <Image src={'/hero-image.jpeg'} width={800} height={600} alt="Screenshot of the collaboration feature" className="shadow-2xl shadow-secondary/20 shadow-ring-10" />
+        </div>
+      </SpotlightCard>
+    </Spotlight>
   )
 }
