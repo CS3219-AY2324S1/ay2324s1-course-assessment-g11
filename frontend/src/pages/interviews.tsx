085b5a37eef52d20c2723fd9d2a092194895bfe9
Charisma Kausar <charisma.kausar@gmail.com>
2023-09-20 05:48:56 +0800

    feat: Add reusable component for Monaco code editor

--- frontend/src/pages/interviews.tsx
+++ frontend/src/pages/interviews.tsx
@@ -1,9 +1,13 @@
-import { TypographyH1 } from '@/components/ui/typography'
+import { TypographyBodyHeavy, TypographyH1 } from '@/components/ui/typography'
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
 
 export default function Interviews() {
   return (
     <div className='min-h-screen p-12'>
-      <TypographyH1 className="text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary w-min">Interviews</TypographyH1>
+      <TypographyH1 className="text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary w-min mb-1">Interviews</TypographyH1>
+      <TypographyBodyHeavy>Try out mock interviews with your peers!</TypographyBodyHeavy>
+      <Link href="/room"><Button variant={"outline"}>Practice with a peer!</Button></Link>
     </div>
   )
 }
