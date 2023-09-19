e1c9f5f84dfb6175d8292a28ce8638ce312a3fde
Charisma Kausar <charisma.kausar@gmail.com>
2023-09-19 16:08:45 +0800

    feat: Add page layout

---
+++ frontend/src/components/footer.tsx
@@ -0,0 +1,6 @@
+export default function Footer() {
+  return (
+    <footer className="flex flex-col items-center justify-center w-full h-24 border-t">
+      </footer>
+  )
+}
---
+++ frontend/src/components/navbar.tsx
@@ -0,0 +1,5 @@
+export default function Navbar() {
+  return (
+    <header></header>
+  )
+}
