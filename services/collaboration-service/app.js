8a8cb52cc04b3baee12c9f52b6defc3cceba09f1
Gabriel Goh <77230723+gycgabriel@users.noreply.github.com>
2023-09-19 13:25:44 +0800

    Room-based collaboration

--- services/collaboration-service/app.js
+++ services/collaboration-service/app.js
@@ -10,9 +10,19 @@ app.get("/", (req, res) => {
 });
 
 io.on("connection", (socket) => {
-  socket.broadcast.emit("hi");
-  socket.on("textchange", (msg) => {
-    io.emit("textchange", msg);
+  console.log("User connected:", socket.id);
+
+  socket.on("join-room", (roomId) => {
+    socket.join(roomId);
+    console.log(socket.id + " joined room:", roomId);
+
+    socket.on("textchange", (text) => {
+      io.to(roomId).emit("textchange", { text });
+    });
+
+    socket.on("disconnect", () => {
+      console.log("User disconnected:", socket.id);
+    });
   });
 });
 
