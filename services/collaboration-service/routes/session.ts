8983559365f99702cba96bb9fbcd03d48cb2c41b
Gabriel Goh <77230723+gycgabriel@users.noreply.github.com>
2023-09-20 22:27:46 +0800

    Typing

--- services/collaboration-service/routes/session.ts
+++ services/collaboration-service/routes/session.ts
@@ -1,15 +1,22 @@
-// @ts-expect-error TS(2451): Cannot redeclare block-scoped variable 'express'.
-const express = require("express");
-// @ts-expect-error TS(2451): Cannot redeclare block-scoped variable 'router'.
-const router = express.Router();
-// @ts-expect-error TS(2580): Cannot find name 'require'. Do you need to install... Remove this comment to see the full error message
-const { v4: uuidv4 } = require("uuid"); // Import the v4 function from the uuid library
+import express, { Request, Response } from 'express';
+import { Socket } from 'socket.io';
+import { io } from '../app';
+import { v4 as uuidv4 } from "uuid";
+
+export const router = express.Router();
+
+interface Session {
+  user1_id: string;
+  user2_id: string;
+  status: "active" | "inactive";
+  text: string;
+}
 
 // Simulated database to store session information
-const sessions = {};
+const sessions: Record<string, Session> = {};
 
 // API to create a collaborative session
-router.post("/create", (req: any, res: any) => {
+router.post("/create", (req: Request, res: Response) => {
   // Extract user information from the request (assuming user1 and user2 IDs)
   const { user1_id, user2_id } = req.body;
 
@@ -20,10 +27,9 @@ router.post("/create", (req: any, res: any) => {
     return res.status(400).json({ error: "Invalid input parameters" });
   }
 
-  const session_id = uuidv4();
+  const session_id: string = uuidv4();
 
   // Create a database entry for the session
-  // @ts-expect-error TS(7053): Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
   sessions[session_id] = {
     user1_id,
     user2_id,
@@ -41,32 +47,31 @@ router.post("/create", (req: any, res: any) => {
 
   // Users to join the room
   const roomName = `session_${session_id}`;
-  // @ts-expect-error TS(2304): Cannot find name 'io'.
-  io.once("connection", (socket: any) => {
+
+  //// Todo - fix this
+  io.once("connection", (socket: Socket) => {
     socket.join(roomName);
     console.log(socket.id + " joined room:" + roomName);
   });
 });
 
 // API to save session information
-router.post("/save", (req: any, res: any) => {
+router.post("/save", (req: Request, res: Response) => {
   try {
     const { session_id, text } = req.body;
     console.log(session_id);
 
-    // @ts-expect-error TS(2360): The left-hand side of an 'in' expression must be a... Remove this comment to see the full error message
-    if (!session_id in sessions) {
+    if (!(session_id in sessions)) {
       return res.status(400).json({ error: "Invalid sessionId provided" });
     }
 
-    // @ts-expect-error TS(7053): Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
     const session = sessions[session_id];
     session.text = text;
     session.status = "active";
 
     res.status(200).json({
       message: "Session saved successfully",
-      session_id: session.session_id,
+      session_id: session_id,
       user1_id: session.user1_id,
       user2_id: session.user2_id,
       status: session.status,
@@ -77,6 +82,3 @@ router.post("/save", (req: any, res: any) => {
     res.status(500).json({ message: "Error saving session" });
   }
 });
-
-// @ts-expect-error TS(2580): Cannot find name 'module'. Do you need to install ... Remove this comment to see the full error message
-module.exports = router;
