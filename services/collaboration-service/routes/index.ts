0904aa900272a016f6d789845fde19efa9509219
Gabriel Goh <77230723+gycgabriel@users.noreply.github.com>
2023-09-21 15:44:52 +0800

    Fix undefined io error when importing index.ts

--- services/collaboration-service/routes/index.ts
+++ services/collaboration-service/routes/index.ts
@@ -6,21 +6,21 @@ export const router = express.Router();
 
 router.get("/", (req: Request, res: Response) => {
   res.sendFile(__dirname + "/index.html");
-});
 
-io.on("connection", (socket: Socket) => {
-  console.log("User connected:", socket.id);
-
-  socket.on("join-room", (roomId: string) => {
-    socket.join(roomId);
-    console.log(socket.id + " joined room:", roomId);
-
-    socket.on("textchange", (text: string) => {
-      io.to(roomId).emit("textchange", { text });
+  io.once("connection", (socket: Socket) => {
+    console.log("User connected:", socket.id);
+  
+    socket.on("join-room", (roomId: string) => {
+      socket.join(roomId);
+      console.log(socket.id + " joined room:", roomId);
+  
+      socket.on("textchange", (text: string) => {
+        io.to(roomId).emit("textchange", { text });
+      });
     });
-  });
-
-  socket.on("disconnect", () => {
-    console.log("User disconnected:", socket.id);
+  
+    socket.on("disconnect", () => {
+      console.log("User disconnected:", socket.id);
+    });
   });
 });
