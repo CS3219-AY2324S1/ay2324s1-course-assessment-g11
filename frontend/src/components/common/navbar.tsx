554932dc27ec4bbcaf058e6bd7660c93d61a6a4a
Charisma Kausar <charisma.kausar@gmail.com>
2023-09-21 13:47:25 +0800

    refactor: Create folders for file org

--- frontend/src/components/common/navbar.tsx
+++ frontend/src/components/common/navbar.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
-import { Button } from "./ui/button";
+import { Button } from "../ui/button";
 import Image from "next/image";
 
 export default function Navbar() {
   return (
     <header>
       <div className="flex py-4 bg-card justify-between items-center px-12">
-        <Link href="/"><Image src="/CodeParty.svg" alt="CodeParty logo" width={125} height={25}/></Link>
+        <Link href="/"><Image src="/common/CodeParty.svg" alt="CodeParty logo" width={125} height={25}/></Link>
         <div className="grid grid-cols-2 gap-4">
           <Link href="/interviews"><Button variant={"outline"}>Log In</Button></Link>
           <Button>Sign Up</Button>
