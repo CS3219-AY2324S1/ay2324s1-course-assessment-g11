35f2d49dbe9946ba110fb1338ebb0a005604cccf
Gabriel Goh <77230723+gycgabriel@users.noreply.github.com>
2023-09-25 16:36:09 +0800

    Fix test url string

--- frontend/src/pages/collab.tsx
+++ frontend/src/pages/collab.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:5001');
+const socket = io('http://localhost:5001/test');
 
 export default function Home() {
   const [text, setText] = useState<string>('');
