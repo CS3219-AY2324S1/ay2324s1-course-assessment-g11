d2d1ab92b596b360529bfabb03cf949e0011baeb
Tay Yi Hsuen <tayyihsuen@gmail.com>
2023-09-30 22:33:42 +0800

    User Service (#46)
    Let's store custom user data in a postgres database.

---
+++ services/user-service/src/app.ts
@@ -0,0 +1,47 @@
+
+import express, {Express} from "express";
+import path from "path";
+import logger from "morgan";
+import indexRouter from "./routes/index";
+import cors from "cors";
+import {io} from "socket.io-client";
+import {eventNames, roomNames} from "../../../event_types/event_definitions";
+
+const port : number = parseInt(process.env.PORT || "5001");
+
+const app : Express = express();
+const gateway_url : string = process.env.GATEWAY_URL || "http://localhost:4000";
+const socket = io(gateway_url);
+
+socket.emit(eventNames.joinEventRoom, roomNames.userRoom);
+
+socket.on(eventNames.userLoggedIn, (userDetails) => {
+  // Call its own API method
+  fetch(`http://localhost:${port}/`, {
+    method: "POST",
+    body: JSON.stringify(userDetails),
+    headers: {
+      'Content-Type': "application/json"
+    }
+  }).then()
+})
+
+socket.on(eventNames.userDeleted, (uidToDelete) => {
+  // Call its own API method
+  fetch(`http://localhost:${port}/${uidToDelete}`, {
+    method: "DELETE"
+  }).then()
+})
+
+app.use(cors())
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use("/", indexRouter);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+export default app;
---
+++ services/user-service/src/db/functions.ts
@@ -0,0 +1,54 @@
+import prismaClient from "./prismaClient";
+
+const userDatabaseFunctions = {
+  async createUser(data : any) {
+    const checkUid = data.uid;
+    const existingUser = await prismaClient.appUser.findUnique({
+      where: {
+        uid: checkUid
+      }
+    })
+
+    if (existingUser) {
+      return null;
+    }
+
+    return prismaClient.appUser.create({
+      data: data
+    }).then((user) => {
+      return user;
+    });
+  },
+
+  async getUserByUid(uid : string) {
+    // Will return null if no such user exists
+    const result = await prismaClient.appUser.findUnique({
+      where: {
+        uid: uid,
+      },
+    });
+    return result;
+  },
+
+  async updateUserByUid(uid : string, data : any) {
+    // Will throw error if user does not exist
+    const updatedResult = await prismaClient.appUser.update({
+      where: {
+        uid: uid
+      },
+      data: data
+    });
+    return updatedResult;
+  },
+
+  async deleteUserByUid(uid : string) {
+    // Will throw error if user does not exist
+    await prismaClient.appUser.delete({
+      where: {
+        uid: uid
+      },
+    });
+  }
+}
+
+export default userDatabaseFunctions;
