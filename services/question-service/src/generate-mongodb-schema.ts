c79f39acf3c7c2916cefc3aba3471e8b2f5cd2ea
Lee Chun Wei <47494777+chunweii@users.noreply.github.com>
2023-09-30 18:45:46 +0800

    Question Service APIs (#23)
    Fixes #8
    
    - [X] Set up MongoDB
    - [X] Create a question
    - [x] Get questions by some fields
    - [x] Get question by object id
    - [x] Update a question by object id
    - [x] Delete a question by object id
    - [x] Get a random question by difficulty and topic

---
+++ services/question-service/src/generate-mongodb-schema.ts
@@ -0,0 +1,113 @@
+import "dotenv/config";
+import { MongoClient, ServerApiVersion } from 'mongodb';
+import { exit } from "process";
+
+const uri = process.env.MONGO_ATLAS_URL || 'mongodb://localhost:27017';
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const mongoClient = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+
+    const db = await mongoClient.connect().then(client => client.db("question_db"));
+
+    db.command( { collMod: "questions",
+    validator: {
+        $jsonSchema: {
+            bsonType: "object",
+            required: [ "topics", "difficulty", "title", "content", "testCaseInputs", "testCaseOutputs", "dateCreated", "dateUpdated" ],
+            properties: {
+                topics: {
+                bsonType: "array",
+                description: "must be an array of strings and is required",
+                items: {
+                    bsonType: "string"
+                }
+                },
+                difficulty: {
+                enum: [ "easy", "medium", "hard" ],
+                description: "must be a string and is required"
+                },
+                title: {
+                bsonType: "string",
+                description: "must be a string and is required"
+                },
+                content: {
+                bsonType: "string",
+                description: "must be a string and is required"
+                },
+                testCaseInputs: {
+                bsonType: "array",
+                description: "must be an array of strings and is required",
+                items: {
+                    bsonType: "string"
+                }
+                },
+                testCaseOutputs: {
+                bsonType: "array",
+                description: "must be an array of strings and is required",
+                items: {
+                    bsonType: "string"
+                }
+                },
+                defaultCode: {
+                bsonType: "object",
+                description: "must be a object and is required",
+                properties: {
+                    java: {
+                        bsonType: "string"
+                    },
+                    cpp: {
+                        bsonType: "string"
+                    },
+                    python: {
+                        bsonType: "string"
+                    }
+                }
+                },
+                solution: {
+                bsonType: "object",
+                description: "must be a object",
+                properties: {
+                    java: {
+                        bsonType: "string"
+                    },
+                    cpp: {
+                        bsonType: "string"
+                    },
+                    python: {
+                        bsonType: "string"
+                    }
+                }
+                },
+                dateCreated: {
+                bsonType: "date",
+                description: "must be a date and is required"
+                },
+                dateUpdated: {
+                bsonType: "date",
+                description: "must be a date and is required"
+                },
+                author: {
+                bsonType: "string"
+                }
+            }
+        }
+    },
+    validationLevel: "strict",
+    } ).then((result) => {
+        console.log(result);
+        exit(0);
+    }).catch((err) => {
+        console.log(err);
+        exit(1);
+    });
+}
+
+run().catch(console.dir);
---
+++ services/question-service/swagger-doc-gen.ts
@@ -0,0 +1,19 @@
+import swaggerAutogen from 'swagger-autogen';
+
+const doc = {
+  info: {
+    title: 'Question Service API',
+    description: 'API for CRUD operations on questions',
+  },
+  host: 'localhost:5002',
+  schemes: ['http'],
+};
+
+const outputFile = './swagger-output.json';
+const endpointsFiles = ['./src/app.ts'];
+
+/* NOTE: if you use the express Router, you must pass in the
+   'endpointsFiles' only the root file where the route starts,
+   such as index.ts, app.ts, routes.js, ... */
+
+swaggerAutogen({openapi: '3.1.0'})(outputFile, endpointsFiles, doc);
