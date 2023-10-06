c76b53cc10235f3ee70c4f3f2b72f76648d46771
Charisma Kausar <charisma.kausar@gmail.com>
2023-10-07 03:28:22 +0800

    feat: Create basic user profile

--- frontend/src/pages/profile.tsx
+++ frontend/src/pages/profile.tsx
@@ -1,5 +1,6 @@
-import { TypographyCode, TypographyH1, TypographyH3 } from "@/components/ui/typography";
-import { use } from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Button } from "@/components/ui/button";
+import { TypographyCode, TypographyH3 } from "@/components/ui/typography";
 
 type UserInfo = {
   name: string
@@ -14,14 +15,25 @@ interface ProfileProps {
 const defaultUser: UserInfo = {
   name: "John Doe",
   username: "johndoe",
-  avatar: "https://avatars.githubusercontent.com/u/1?v=4"
+  avatar: "https://github.com/shadcn.png"
 }
 
 export default function Profile({ userInfo = defaultUser }: ProfileProps) {
   return (
-    <div className="max-w-7xl mx-auto">
-      <TypographyH3>{userInfo?.name}</TypographyH3>
-      <TypographyCode>@{userInfo?.username}</TypographyCode>
+    <div className="max-w-7xl mx-auto flex flex-col justify-center items-center">
+      <div className="max-w-sm m-4">
+        <div className="flex items-center w-full justify-center gap-x-4 p-4">
+          <Avatar className="h-24 w-24">
+            <AvatarImage src={userInfo.avatar} />
+            <AvatarFallback>{userInfo.name.charAt(0).toUpperCase()}</AvatarFallback>
+          </Avatar>
+          <div>
+            <TypographyH3>{userInfo?.name}</TypographyH3>
+            <TypographyCode>@{userInfo?.username}</TypographyCode>
+          </div>
+        </div>
+        <Button className="w-full" variant="secondary">Edit Profile</Button>
+      </div>
     </div>
   )
 }
