1fad73d58c1b2aa0c1f06f6f71d3bb8efb91c80a
Charisma Kausar <charisma.kausar@gmail.com>
2023-10-07 03:07:12 +0800

    chore: Install avatar component

--- frontend/src/pages/profile.tsx
+++ frontend/src/pages/profile.tsx
@@ -1,3 +1,27 @@
-export default function Profile() {
-  
+import { TypographyCode, TypographyH1, TypographyH3 } from "@/components/ui/typography";
+import { use } from "react";
+
+type UserInfo = {
+  name: string
+  username: string
+  avatar: string
+}
+
+interface ProfileProps {
+  userInfo: UserInfo
+}
+
+const defaultUser: UserInfo = {
+  name: "John Doe",
+  username: "johndoe",
+  avatar: "https://avatars.githubusercontent.com/u/1?v=4"
+}
+
+export default function Profile({ userInfo = defaultUser }: ProfileProps) {
+  return (
+    <div className="max-w-7xl mx-auto">
+      <TypographyH3>{userInfo?.name}</TypographyH3>
+      <TypographyCode>@{userInfo?.username}</TypographyCode>
+    </div>
+  )
 }
