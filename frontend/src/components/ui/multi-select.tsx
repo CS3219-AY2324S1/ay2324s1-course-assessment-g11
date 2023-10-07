80f2b94e8db16441716669fa581056f22a5fed9e
Charisma Kausar <charisma.kausar@gmail.com>
2023-10-07 22:10:43 +0800

    feat: Complete add question form without code editor

--- frontend/src/components/ui/multi-select.tsx
+++ frontend/src/components/ui/multi-select.tsx
@@ -46,7 +46,7 @@ function MultiSelect({ options, selected, onChange, className, ...props }: Multi
           variant="outline"
           role="combobox"
           aria-expanded={open}
-          className={`w-full justify-between ${selected.length > 1 ? "h-full" : "h-10"}`}
+          className={`w-full justify-between border-[1] ${selected.length > 1 ? "h-full" : "h-10"}`}
           onClick={() => setOpen(!open)}
         >
           <div className="flex gap-1 flex-wrap">
--- frontend/src/pages/questions/new.tsx
+++ frontend/src/pages/questions/new.tsx
@@ -16,9 +16,21 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form"
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
+
 import DifficultySelector from "@/components/common/difficulty-selector";
 import { MultiSelect } from "@/components/ui/multi-select";
 import { Textarea } from "@/components/ui/textarea";
+import { Switch } from "@/components/ui/switch";
+import { useState } from "react";
 
 const formSchema = z.object({
   title: z.string().min(2).max(100),
@@ -26,10 +38,12 @@ const formSchema = z.object({
   tags: z.array(z.string().min(2).max(100)),
   description: z.string().min(2).max(1000),
   language: z.enum(['javascript', 'python', 'java', 'c++']),
-  code: z.string().min(2).max(10000) || undefined,
+  code: z.string().min(0).max(10000) || undefined,
 })
 
 export default function QuestionForm() {
+  const [hasSolution, setHasSolution] = useState(false)
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -88,7 +102,7 @@ export default function QuestionForm() {
         <TypographyH2>Add a Question</TypographyH2>
       </div>
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 my-8">
           <FormField
             control={form.control}
             name="title"
@@ -148,7 +162,65 @@ export default function QuestionForm() {
               </FormItem>
             )}
           />
-          <Button type="submit">Submit</Button>
+          <FormItem className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
+            <div className="space-y-0.5">
+              <FormLabel>Solution</FormLabel>
+              <FormDescription>
+                Include solution for this question
+              </FormDescription>
+            </div>
+            <FormControl>
+              <Switch
+                checked={hasSolution}
+                onCheckedChange={setHasSolution}
+              />
+            </FormControl>
+          </FormItem>
+          {hasSolution && (
+            <FormField
+              control={form.control}
+              name="language"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Language Used</FormLabel>
+                  <FormControl>
+                    <Select>
+                      <SelectTrigger className="w-[180px]">
+                        <SelectValue placeholder="Theme" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectItem value="light">Light</SelectItem>
+                        <SelectItem value="dark">Dark</SelectItem>
+                        <SelectItem value="system">System</SelectItem>
+                      </SelectContent>
+                    </Select>
+
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          )}
+          {hasSolution && (
+            <FormField
+              control={form.control}
+              name="code"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Solution Code</FormLabel>
+                  <FormControl>
+                    <Textarea
+                      placeholder="Write your code here."
+                      className="resize-none"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          )}
+          <Button type="submit">Add Question</Button>
         </form>
       </Form>
     </div>
