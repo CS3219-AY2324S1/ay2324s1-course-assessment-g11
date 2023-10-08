e534d3fadb6f0963f159b5c53fab9b696a2943b7
chunweii <47494777+chunweii@users.noreply.github.com>
2023-10-08 14:44:43 +0800

    Add 60 second timeout

--- services/matching-service/src/app.ts
+++ services/matching-service/src/app.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import logger from "morgan";
 import { Server } from "socket.io";
+import { io as ioClient } from "socket.io-client";
 import matchingRoutes from "./routes/matchingRoutes";
 import prisma from "./prismaClient";
+import { Match, Prisma } from "@prisma/client";
+import EventEmitter from "events";
 
 const app = express();
 const port = process.env.PORT || 5002;
@@ -16,6 +19,8 @@ const io = new Server(httpServer);
 
 app.set("io", io);
 
+const MAX_WAITING_TIME = 60 * 1000; // 60 seconds
+
 type UserMatchReq = {
   userId: string;
   difficulties: string[];
@@ -28,37 +33,54 @@ const userQueuesByProgrammingLanguage: { [language: string]: UserMatchReq[] } = 
   "cpp": []
 };
 
-const waitingUsers: Map<string, string> = new Map(); // key: user id, val: socket id
-
-const matchedUsers: Map<string, {username: string, socketId: string}> = new Map(); // key: user id, val: socket id
+const waitingUsers: Map<string, EventEmitter> = new Map(); // key: user id, val: Event
 
 io.on("connection", (socket) => {
-  console.log(`User connected: ${socket.id}`);
+  let userId = socket.handshake.query.username as string || "";
+  console.log(`User connected: ${socket.id} and username ${userId}`);
 
   let userMatchReq: UserMatchReq = {
-    userId: "",
+    userId: userId,
     difficulties: [],
     programmingLang: "python"
   };
 
+  prisma.match.findFirst({
+    where: {
+      OR: [
+        { userId1: userId },
+        { userId2: userId }
+      ]
+    }
+  }).then(existingMatch => {
+    if (existingMatch) {
+      console.log(`User ${userId} is already matched with user ${existingMatch.userId1 === userId ? existingMatch.userId2 : existingMatch.userId1}`);
+      socket.emit("error", "You are already matched with someone.");
+      socket.join(existingMatch.roomId);
+      socket.emit("matchFound", existingMatch);
+    }
+  });
+  let timer = setTimeout(() => { }, 0);
+
   socket.on("disconnect", () => {
     console.log(`User disconnected: ${socket.id}`);
     // Remove user from queue if they disconnect
+    clearTimeout(timer);
     if (waitingUsers.has(userMatchReq.userId)) {
       console.log(`User ${userMatchReq.userId} disconnected while waiting for a match`);
       userQueuesByProgrammingLanguage[userMatchReq.programmingLang] = userQueuesByProgrammingLanguage[userMatchReq.programmingLang].filter(user => user.userId !== userMatchReq.userId);
+      waitingUsers.get(userMatchReq.userId)?.removeAllListeners();
       waitingUsers.delete(userMatchReq.userId);
     }
-    if (matchedUsers.has(userMatchReq.userId)) {
-      console.log(`User ${userMatchReq.userId} disconnected while in a match`);
-      const matchingUser = matchedUsers.get(userMatchReq.userId);
-      io.to(matchingUser?.socketId || "").emit("matchLeft", userMatchReq.userId);
-      matchedUsers.delete(userMatchReq.userId);
-      matchedUsers.delete(matchingUser?.username || "");
-    }
+    // Match should not be cancelled since the user might reconnect
   });
 
   socket.on("lookingForMatch", async (userId: string, difficulties: string[], programmingLang: string) => {
+    if (!userId || !difficulties || !programmingLang) {
+      console.log(`Invalid request from user ${userId}`);
+      socket.emit("error", "Invalid request");
+      return;
+    }
     if (waitingUsers.has(userMatchReq.userId)) {
       console.log(`User ${userId} is already in the queue`);
       return;
@@ -69,15 +91,28 @@ io.on("connection", (socket) => {
     } else if (!userMatchReq.userId) {
       userMatchReq.userId = userId;
     }
+
+    const existingMatch = await prisma.match.findFirst({
+      where: {
+        OR: [
+          { userId1: userId },
+          { userId2: userId }
+        ]
+      }
+    });
+
+    if (existingMatch) {
+      console.log(`User ${userId} is already matched with user ${existingMatch.userId1 === userId ? existingMatch.userId2 : existingMatch.userId1}`);
+      socket.emit("error", "You are already matched with someone.");
+      socket.join(existingMatch.roomId);
+      socket.emit("matchFound", existingMatch);
+      return;
+    }
+
     userMatchReq.difficulties = difficulties;
     userMatchReq.programmingLang = programmingLang;
 
     console.log(`User ${userId} is looking for a match with difficulties ${difficulties} and programming language ${programmingLang}`);
-
-    // Add user to the queue
-    userQueuesByProgrammingLanguage[programmingLang].push({ userId: userId, difficulties, programmingLang });
-    // usersQueue.push(userId);
-    waitingUsers.set(userId, socket.id);
 
     // Attempt to find a match for the user
     const matchedUser = userQueuesByProgrammingLanguage[programmingLang]
@@ -85,15 +120,22 @@ io.on("connection", (socket) => {
         userMatchReq.difficulties.filter(v => difficulties.includes(v)));
     const matchId = matchedUser?.userId;
     const difficulty = matchedUser?.difficulties.find(v => difficulties.includes(v));
+
     if (matchId) {
       console.log(`Match found for user ${userId} with user ${matchId} and difficulty ${difficulty}`);
 
       // Inform both users of the match
-      socket.emit("matchFound", userId, matchId);
-      const matchingSocketId = waitingUsers.get(matchId)?.toString() || "";
-      matchedUsers.set(userId, {username: matchId, socketId: matchingSocketId});
-      matchedUsers.set(matchId, {username: userId, socketId: socket.id});
-      io.to(matchingSocketId).emit("matchFound", matchId, userId);
+      const newMatch = await prisma.match.create({
+        data: {
+          userId1: userId,
+          userId2: matchId,
+          chosenDifficulty: difficulty || "easy",
+          chosenProgrammingLanguage: programmingLang
+        }
+      });
+      waitingUsers.get(matchId)?.emit("matchFound", newMatch);
+      socket.emit("matchFound", newMatch);
+      socket.join(newMatch.roomId);
 
       // Update the database with the matched users (pseudo-code)
       await prisma.user.update({
@@ -107,17 +149,36 @@ io.on("connection", (socket) => {
       });
 
       // Remove both users from the queue
-      userQueuesByProgrammingLanguage[programmingLang] = userQueuesByProgrammingLanguage[programmingLang].filter(user => user.userId !== userId && user.userId !== matchId);
-      waitingUsers.delete(userId);
+      userQueuesByProgrammingLanguage[programmingLang] = userQueuesByProgrammingLanguage[programmingLang].filter(user => user.userId !== matchId && user.userId !== userId);
       waitingUsers.delete(matchId);
+      waitingUsers.delete(userId);
 
     } else {
-      console.log(`No match found for user ${userId} yet.`);
+      // Add user to the queue
+      userQueuesByProgrammingLanguage[programmingLang].push({ userId: userId, difficulties, programmingLang });
+      let event = new EventEmitter();
+      waitingUsers.set(userId, event);
+      event.on("matchFound", (match: Match) => {
+        console.log(`Match found for user ${userId} with user ${match.userId1 === userId ? match.userId2 : match.userId1} and difficulty ${match.chosenDifficulty}`);
+        socket.join(match.roomId);
+        socket.emit("matchFound", match);
+        clearTimeout(timer);
+      });
+      setTimeout(() => {
+        if (waitingUsers.has(userId)) {
+          console.log(`No match found for user ${userId} yet.`);
+          userQueuesByProgrammingLanguage[programmingLang] = userQueuesByProgrammingLanguage[programmingLang].filter(user => user.userId !== userId);
+          waitingUsers.delete(userId);
+          socket.emit("matchNotFound");
+        }
+      }, MAX_WAITING_TIME);
+      console.log(`Queueing user ${userId}.`);
     }
   });
 
   socket.on("cancelLooking", async () => {
     console.log(`User ${userMatchReq.userId} is no longer looking for a match`);
+    clearTimeout(timer);
     userQueuesByProgrammingLanguage[userMatchReq.programmingLang] = userQueuesByProgrammingLanguage[userMatchReq.programmingLang].filter(user => user.userId !== userMatchReq.userId);
     waitingUsers.delete(userMatchReq.userId);
   })
@@ -125,16 +186,27 @@ io.on("connection", (socket) => {
   socket.on("leaveMatch", async () => {
     console.log(`User ${userMatchReq.userId} has left the match`);
 
-    const user = await prisma.user.findUnique({ where: { id: userMatchReq.userId } });
+    const match = await prisma.match.findFirst({
+      where: {
+        OR: [
+          { userId1: userMatchReq.userId },
+          { userId2: userMatchReq.userId }
+        ]
+      }
+    });
 
-    if (user?.matchedUserId) {
+    if (match) {
       // Notify the matched user
-      const matchingUser = matchedUsers.get(userMatchReq.userId);
-      console.log(`Notifying user ${user.matchedUserId} that user ${userMatchReq.userId} has left the match using socket ${matchingUser?.socketId}`);
-      io.to(matchingUser?.socketId || "").emit("matchLeft", userMatchReq.userId);
-      
-      matchedUsers.delete(userMatchReq.userId);
-      matchedUsers.delete(matchingUser?.username || "");
+      await prisma.match.delete(
+        {
+          where: {
+            roomId: match?.roomId
+          }
+        }
+      );
+      const matchingUserId = match?.userId1 === userMatchReq.userId ? match?.userId2 : match?.userId1;
+      console.log(`Notifying user ${matchingUserId} that user ${userMatchReq.userId} has left the match`);
+      io.to(match.roomId).emit("matchLeft", match);
 
       // Update database to remove matchedUserId for both users
       await prisma.user.update({
@@ -142,20 +214,34 @@ io.on("connection", (socket) => {
         data: { matchedUserId: null },
       });
       await prisma.user.update({
-        where: { id: user.matchedUserId },
+        where: { id: matchingUserId },
         data: { matchedUserId: null },
       });
     }
   });
 
   socket.on("sendMessage", async (userId: string, message: string) => {
+    if (!userId || !message) {
+      console.log(`Invalid request from user ${userId}`);
+      socket.emit("error", "Invalid request");
+      return;
+    }
     console.log(`User ${userId} sent a message: ${message}`);
 
-    const user = await prisma.user.findUnique({ where: { id: userId } });
+    const match = await prisma.match.findFirst({
+      where: {
+        OR: [
+          { userId1: userId },
+          { userId2: userId }
+        ]
+      }
+    });
 
-    if (user?.matchedUserId) {
+    const matchedUser = match?.userId1 === userId ? match?.userId2 : match?.userId1;
+
+    if (matchedUser) {
       // Forward the message to the matched user
-      io.to(matchedUsers.get(user.matchedUserId)?.socketId || "").emit(
+      socket.to(match?.roomId || "").emit(
         "receiveMessage",
         userId,
         message
