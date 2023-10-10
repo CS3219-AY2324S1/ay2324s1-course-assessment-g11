d73cf32a700de27ef091d211862c7d338255b66f
Gabriel Goh <77230723+gycgabriel@users.noreply.github.com>
2023-10-10 23:19:53 +0800

    Add common shared-ot

---
+++ utils/shared-ot.ts
@@ -0,0 +1,33 @@
+import { diff_match_patch } from "diff-match-patch";
+import { insert, remove, TextOp } from "ot-text-unicode";
+
+export interface TextOperationSet {
+  version: number;
+  operations: TextOp;
+}
+
+export function createTextOpFromTexts(
+  prevText: string,
+  currentText: string
+): TextOp {
+  const dmp = new diff_match_patch();
+  const diffs = dmp.diff_main(prevText, currentText);
+  //dmp.diff_cleanupSemantic(diffs);
+
+  var textop: TextOp = [];
+
+  var skipn: number = 0;
+
+  for (const [operation, text] of diffs) {
+    if (operation === 0) {
+      skipn += text.length;
+    } else if (operation === -1) {
+      textop = [...textop, ...remove(skipn, text)];
+      skipn = 0;
+    } else {
+      textop = [...textop, ...insert(skipn, text)];
+      skipn = 0;
+    }
+  }
+  return textop;
+}
