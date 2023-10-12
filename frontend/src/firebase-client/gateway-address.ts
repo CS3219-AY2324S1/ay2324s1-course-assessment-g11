e15fcad4bc4bd6e5014a67726b42c366c8918767
YIHSUEN\Yi Hsuen <tayyihsuen@gmail.com>
2023-10-12 20:14:32 +0800

    Change usage of env variables in frontend

--- frontend/src/firebase-client/gateway-address.ts
+++ frontend/src/firebase-client/gateway-address.ts
@@ -2,13 +2,10 @@
  * File for defining the address of the gateway server.
  *
  * How to use:
- *   - For development environment, use localhost
- *   - For production environment, use DNS
+ *   - Leave GATEWAY_ADDRESS empty for dev environments
+ *   - For prod, pass in a separate address to GATEWAY_ADDRESS
  */
-const gatewayAddress =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:4000/"
-    : "http://gateway:4000/";
+const gatewayAddress = process.env.GATEWAY_ADDRESS || "http://localhost:4000/"
 
 export const userApiPathAddress = gatewayAddress + "api/user-service/";
 export const questionApiPathAddress = gatewayAddress + "api/question-service/";
