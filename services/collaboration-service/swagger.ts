b14773bb63c5b00d1318bbdbab6b976d7d793564
Gabriel Goh <77230723+gycgabriel@users.noreply.github.com>
2023-10-14 16:37:32 +0800

    Collaboration service real time (#57)
    - Operational Transformations in Collaboration Service
    - Add utils/shared-ot.ts
      - next.config.js: Enable experimental feature - external dir
      - Some scary warning appears here
    - Fix frontend
      - remove debouncer
    - Add cursor support, defaults to 0
    
    Current design:
    
    Client --> Server: OT + Cursor
    Server --> Client: Full text + Version number + Cursor
    
    Version number incremented by Server, client should not increment
    because version number refers to previous version that is edited upon.
    
    Client waits for: 
    - Acknowledgement from server of prev request
    - Sync version (and text) from server
    before they can send the next update request
    
    This ensures no conflicting versions and mix-ups.

--- services/collaboration-service/swagger.ts
+++ services/collaboration-service/swagger.ts
@@ -6,7 +6,7 @@ const doc = {
     description:
       "Provides the mechanism for real-time collaboration (e.g., concurrent code editing) between the authenticated and matched users in the collaborative space",
   },
-  host: "localhost:5001",
+  host: "localhost:5003",
   schemes: ["http"],
 };
 
@@ -17,9 +17,9 @@ const endpointsFiles = ["./src/app.ts"];
    'endpointsFiles' only the root file where the route starts,
    such as index.js, app.js, routes.js, ... */
 
-swaggerAutogen({ openapi: "3.0.0" })(outputFile, endpointsFiles, doc)
-  /*.then(
+swaggerAutogen({ openapi: "3.0.0" })(outputFile, endpointsFiles, doc);
+/*.then(
     async () => {
       await import("./src/app"); // Your project's root file
     }
-  );*/  // to run it after swagger-autogen
+  );*/ // to run it after swagger-autogen
