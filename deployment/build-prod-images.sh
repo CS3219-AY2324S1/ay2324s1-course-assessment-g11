5ae465824dbd0747cbe4997836a8b14af354efce
YIHSUEN\Yi Hsuen <tayyihsuen@gmail.com>
2023-10-17 22:42:31 +0800

    Fix image build bugs

--- deployment/build-prod-images.sh
+++ deployment/build-prod-images.sh
@@ -2,12 +2,16 @@
 docker build -t peerprep-base -f ../Dockerfile ..
 
 # Create array of services
+declare -a service_array
 service_array=("admin-service" "collaboration-service" "gateway" "matching-service" "question-service" "user-service" "frontend")
 
 # Build and publish prod images with context set to be parent directory
-for s in ${service_array[@]}; do
-  docker build \
-    --tag $GKE_REGION-docker.pkg.dev/$PROJECT_ID/$ARTIFACT_REPOSITORY_NAME/${service_array[s]}:latest \
-    --file prod-dockerfiles/Dockerfile.${service_array[s]}-prod ..
-  docker push $GKE_REGION-docker.pkg.dev/$PROJECT_ID/$ARTIFACT_REPOSITORY_NAME/${service_array[s]}:latest
+for i in ${!service_array[@]}; do
+#  docker build \
+#    --tag $GKE_REGION-docker.pkg.dev/$PROJECT_ID/$ARTIFACT_REPOSITORY_NAME/${service_array[$s]}:latest \
+#    --file prod-dockerfiles/Dockerfile.${service_array[$s]}-prod ..
+  #docker push $GKE_REGION-docker.pkg.dev/$PROJECT_ID/$ARTIFACT_REPOSITORY_NAME/${service_array[$s]}:latest
+    docker build \
+      --tag ${service_array[$i]}:latest \
+      --file prod-dockerfiles/Dockerfile.${service_array[$i]}-prod ..
 done
