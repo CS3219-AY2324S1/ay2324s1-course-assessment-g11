f5e79d62ef6e9cc06f1d4be4dd8e11efa942d66b
YIHSUEN\Yi Hsuen <tayyihsuen@gmail.com>
2023-10-21 16:08:48 +0800

    Remove console log statement

--- services/gateway/src/auth/firebase.ts
+++ services/gateway/src/auth/firebase.ts
@@ -42,7 +42,6 @@ export function promiseVerifyIsAdmin(idToken: string) {
   return firebaseAuth
     .verifyIdToken(idToken, true)
     .then((claims) => {
-      console.log(claims)
       return !!claims.admin;
     })
     .catch((error) => {
