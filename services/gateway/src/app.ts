2f31bb241a68f9c29712f1c128276e9cc4a30a5c
YIHSUEN\Yi Hsuen <tayyihsuen@gmail.com>
2023-10-25 09:12:42 +0800

    Separate ws and http proxy ports in gateway

--- services/gateway/src/app.ts
+++ services/gateway/src/app.ts
@@ -3,34 +3,58 @@ import cors from 'cors';
 import { setupLogging } from "./logging/logging";
 import { setupAdmin, setupUserIdMatch, setupIsLoggedIn } from "./auth/auth";
 import { setupProxies } from "./proxy/proxy";
-import { proxied_routes } from "./proxied_routes/proxied_routes";
+import {http_proxied_routes, wsCollaborationProxiedRoutes, wsMatchProxiedRoutes} from "./proxied_routes/proxied_routes";
 import {frontendAddress} from "./proxied_routes/service_names";
+import {createProxyMiddleware} from "http-proxy-middleware";
 
 
-const app : Express = express();
+const httpApp : Express = express();
+const wsMatchApp : Express = express();
+const wsCollaborationApp : Express = express();
+
 const corsOptions = {
   origin: frontendAddress,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
 }
 
-const port : number = parseInt(process.env.PORT || "4000");
+const httpProxyPort : number = parseInt(process.env.HTTP_PROXY_PORT || "4000");
+const wsMatchProxyPort : number = parseInt(process.env.WS_MATCH_PROXY_PORT || "4002");
+const wsCollaborationProxyPort : number = parseInt(process.env.WS_COLLABORATION_PROXY_PORT || "4003");
 
-app.use(cors(corsOptions))
+httpApp.use(cors(corsOptions));
+wsMatchApp.use(cors(corsOptions));
+wsCollaborationApp.use(cors(corsOptions));
 
 /**
  * WARNING: Do not add body parsing middleware to the Gateway.
  * Otherwise, proxying POST requests with request body would not work.
  */
-setupLogging(app);
-setupIsLoggedIn(app, proxied_routes);
-setupUserIdMatch(app, proxied_routes);
-setupAdmin(app, proxied_routes);
-const proxyMiddlewareArray = setupProxies(app, proxied_routes);
+setupLogging(httpApp);
+setupLogging(wsMatchApp);
+setupLogging(wsCollaborationApp);
 
-const server = app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+setupIsLoggedIn(httpApp, http_proxied_routes);
+setupIsLoggedIn(wsMatchApp, wsMatchProxiedRoutes);
+setupIsLoggedIn(wsCollaborationApp, wsCollaborationProxiedRoutes);
+
+
+setupUserIdMatch(httpApp, http_proxied_routes);
+setupAdmin(httpApp, http_proxied_routes);
+setupProxies(httpApp, http_proxied_routes);
+
+const wsMatchProxyMiddleware = createProxyMiddleware(wsMatchProxiedRoutes[0].proxy);
+wsMatchApp.use(wsMatchProxiedRoutes[0].url, wsMatchProxyMiddleware);
+const wsCollaborationProxyMiddleware = createProxyMiddleware(wsCollaborationProxiedRoutes[0].proxy);
+wsCollaborationApp.use(wsCollaborationProxiedRoutes[0].url, wsCollaborationProxyMiddleware);
+
+httpApp.listen(httpProxyPort, () => {
+  console.log(`Gateway HTTP proxy listening on port ${httpProxyPort}`);
 })
 
-for (let i = 0; i < proxyMiddlewareArray.length; i++) {
-  server.on('upgrade', proxyMiddlewareArray[i].upgrade);
-}
+wsMatchApp.listen(wsMatchProxyPort, () => {
+  console.log(`Gateway WebSockets Match Proxy listening on port ${wsMatchProxyPort}`);
+})
+
+wsCollaborationApp.listen(wsCollaborationProxyPort, () => {
+  console.log(`Gateway WebSockets Collaboration Proxy listening on port ${wsCollaborationProxyPort}`);
+})
