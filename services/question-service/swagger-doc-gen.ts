45d0346b28079c90fd0d3688a74d9c8db056ef13
Gabriel Goh <77230723+gycgabriel@users.noreply.github.com>
2023-10-26 20:40:45 +0800

    Fix swagger port

--- services/question-service/swagger-doc-gen.ts
+++ services/question-service/swagger-doc-gen.ts
@@ -1,19 +1,19 @@
-import swaggerAutogen from 'swagger-autogen';
+import swaggerAutogen from "swagger-autogen";
 
 const doc = {
   info: {
-    title: 'Question Service API',
-    description: 'API for CRUD operations on questions',
+    title: "Question Service API",
+    description: "API for CRUD operations on questions",
   },
-  host: 'localhost:5002',
-  schemes: ['http'],
+  host: "localhost:5004",
+  schemes: ["http"],
 };
 
-const outputFile = './swagger-output.json';
-const endpointsFiles = ['./src/app.ts'];
+const outputFile = "./swagger-output.json";
+const endpointsFiles = ["./src/app.ts"];
 
 /* NOTE: if you use the express Router, you must pass in the
    'endpointsFiles' only the root file where the route starts,
    such as index.ts, app.ts, routes.js, ... */
 
-swaggerAutogen({openapi: '3.1.0'})(outputFile, endpointsFiles, doc);
+swaggerAutogen({ openapi: "3.1.0" })(outputFile, endpointsFiles, doc);
