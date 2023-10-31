cbb9cb4e6af2434a20bfffa9d3f270f97fab7aa8
Gabriel Goh <77230723+gycgabriel@users.noreply.github.com>
2023-10-31 21:53:02 +0800

    Add dev local for colab service

--- start-app-no-docker.sh
+++ start-app-no-docker.sh
@@ -11,7 +11,7 @@ prepend() {
     (yarnpkg workspace frontend dev:local | prepend "frontend: ") & \
     (yarnpkg workspace user-service dev:local | prepend "user-service: ") & \
     (yarnpkg workspace admin-service dev:local | prepend "admin-service: ") & \
-    (yarnpkg workspace collaboration-service dev | prepend "collaboration-service: ") & \
+    (yarnpkg workspace collaboration-service dev:local | prepend "collaboration-service: ") & \
     (yarnpkg workspace matching-service dev:local | prepend "matching-service: ") & \
     (yarnpkg workspace question-service dev:local | prepend "question-service: ") & \
     (yarnpkg workspace gateway dev:local | prepend "gateway: ") & \
