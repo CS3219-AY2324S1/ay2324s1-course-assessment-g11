8afd0575bc7661e6d500fce2d3111ff2f4054ca5
Ong Jun Xiong <junxiongong2@gmail.com>
2023-11-02 22:07:43 +0800

    remove redundant matching details

--- services/matching-service/src/routes/matchingRoutes.ts
+++ services/matching-service/src/routes/matchingRoutes.ts
@@ -1,16 +1,8 @@
 import express from "express";
-import {
-  findMatch,
-  getMatch,
-  leaveMatch,
-  updateMatchQuestion,
-} from "../controllers/matchingController";
+import { updateMatchQuestion } from "../controllers/matchingController";
 
 const router = express.Router();
 
-router.get("/:userId/findMatch", findMatch);
-router.post("/:userId/leave", leaveMatch);
-router.get("/match/:room_id", getMatch);
 router.patch("/match/:room_id", updateMatchQuestion);
 router.get("/demo", (req, res) => res.sendFile(__dirname + "/index.html"));
 
