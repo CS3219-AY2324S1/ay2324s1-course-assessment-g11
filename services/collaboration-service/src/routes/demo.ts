e79b8d576f0e83ffb527bc5f6e77c212e1aa4bc4
Gabriel Goh <77230723+gycgabriel@users.noreply.github.com>
2023-11-12 17:03:24 +0800

    Delete collab service, deployment and gateway (#243)

--- services/collaboration-service/src/routes/demo.ts
+++
@@ -1,9 +0,0 @@
-import express, { Request, Response } from "express";
-
-const router = express.Router();
-
-router.get("/", (req: Request, res: Response) => {
-  res.sendFile(__dirname + "/demo.html");
-});
-
-export default router;
