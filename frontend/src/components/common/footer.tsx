b1570cbb79991449975940316070be528bfcdcc8
Charisma Kausar <charisma.kausar@gmail.com>
2023-11-14 00:37:00 +0800

    misc: delete frontend directory

--- frontend/src/components/common/footer.tsx
+++
@@ -1,9 +0,0 @@
-import { TypographyLink } from "../ui/typography";
-
-export default function Footer() {
-  return (
-    <footer className="flex flex-col items-center justify-center w-full h-36 bg-accent">
-      <TypographyLink href="https://github.com/CS3219-AY2324S1/ay2324s1-course-assessment-g11" >Source Code</TypographyLink>
-    </footer>
-  )
-}
