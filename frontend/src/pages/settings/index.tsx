1518b877ab0bf6a98c168ef9e4a677e092dc385c
YIHSUEN\Yi Hsuen <tayyihsuen@gmail.com>
2023-11-18 00:46:49 +0800

    Block AccountSettingsCard from being shown if not logged in

--- frontend/src/pages/settings/index.tsx
+++ frontend/src/pages/settings/index.tsx
@@ -4,16 +4,20 @@ import { TypographyH1 } from "@/components/ui/typography";
 
 import { useRouter } from "next/router";
 import AccountSettingsCard from "./_account";
+import {useContext} from "react";
+import {AuthContext} from "@/contexts/AuthContext";
 
 export default function Settings() {
   const router = useRouter();
+  const { user: currentUser } = useContext(AuthContext);
+
 
   return (
     <div className="max-w-7xl mx-auto py-10">
       <div className="flex">
         <div className="w-full p-10 m-10 flex flex-col gap-10">
           <TypographyH1>Settings</TypographyH1>
-          <AccountSettingsCard />
+          {currentUser && <AccountSettingsCard />}
           <Button
             className="w-full gap-2"
             onClick={() => router.push("/profile")}
